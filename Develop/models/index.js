da2f3cf4d3fe82beeade656d6bed5ef97532e653
AdrianoA <adriano.armendaris@gmail.com>
2021-08-22 01:06:08 -0500

    models relationship defined

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -10,11 +10,30 @@ Product.belongsTo(Category, {
 });
 
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: "product_id",
+  through: {
+    model: "product_tag",
+    unique: false,
+  },
+  as: "TagAlias",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: "tag_id",
+  through: {
+    model: "product_tag",
+    unique: false,
+  },
+  as: "ProductAlias",
+});
 
 module.exports = {
   Product,
